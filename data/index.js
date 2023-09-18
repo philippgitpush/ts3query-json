921c703e6942c91eea422afb69ed1c1c109c6eaa
philfiru <adeleine1412@gmail.com>
2023-09-18 23:07:26 +0200

    Keep connection alive to avoid rate limits

--- data/index.js
+++ data/index.js
@@ -1,6 +1,7 @@
 const { TeamSpeak, QueryProtocol } = require("ts3-nodejs-library");
 const http = require("http");
 
+// Environment
 const ts3_username = process.env.TS3_USERNAME;
 const ts3_password = process.env.TS3_PASSWORD;
 const ts3_host = process.env.TS3_HOST;
@@ -8,32 +9,35 @@ const ts3_queryport = process.env.TS3_QUERYPORT;
 const ts3_serverport = process.env.TS3_SERVERPORT;
 const web_serverport = process.env.WEB_SERVERPORT;
 
-async function getTS3Info() {
-  try {
-    const teamspeak = await TeamSpeak.connect({
-      host: ts3_host,
-      queryport: ts3_queryport,
-      serverport: ts3_serverport,
-      username: ts3_username,
-      password: ts3_password,
-      nickname: "TS3Query",
-    });
+// TeamSpeak
+const teamspeak = new TeamSpeak({
+  host: ts3_host,
+  queryport: ts3_queryport,
+  serverport: ts3_serverport,
+  username: ts3_username,
+  password: ts3_password,
+  nickname: "TS3Query"
+})
 
-    const serverInfo = await teamspeak.serverInfo();
-    const clientsInfo = await teamspeak.clientList({ clientType: 0 });
-    const customInfo = getCustomInfo(clientsInfo, serverInfo);
+let reconnectAttempts = 0;
 
-    teamspeak.forceQuit();
+teamspeak.on("ready", () => {
+  console.error("ServerQuery connection established.");
+  reconnectAttempts = 0;
+})
 
-    return { serverInfo, customInfo };
-  } catch (e) {
-    console.error("Error connecting to TeamSpeak server:");
-    console.error(e);
-    throw e;
-  } 
+teamspeak.on("error", async () => { retryConnection() });
+
+async function retryConnection() {
+  reconnectAttempts++;
+  console.error("(Attempt " + reconnectAttempts + ") Error connecting to TeamSpeak server. Trying again in 60 seconds ...");
+  setTimeout(async () => { try { await teamspeak.reconnect() } catch (reconnectError) {} }, 60000);
 }
 
-function getCustomInfo(clientsInfo, serverInfo) {
+async function requestServerInfo() {
+  const serverInfo = await teamspeak.serverInfo();
+  const clientsInfo = await teamspeak.clientList({ clientType: 0 });
+
   const uptime_seconds = serverInfo.virtualserverUptime;
   const uptime_days = Math.floor(uptime_seconds / (60 * 60 * 24));
   const uptime_remainder_hours = Math.floor((uptime_seconds % (60 * 60 * 24)) / (60 * 60));
@@ -57,34 +61,19 @@ function getCustomInfo(clientsInfo, serverInfo) {
     serverClientsFormatted: "(" + clientsInfo.length + ") " + onlineUsersNicknames
   };
 
-  return customInfo;
+  return { serverInfo, customInfo };
 }
 
+// Webserver
 const server = http.createServer(async (req, res) => {
   if (req.url === "/") {
-    try {
-
-      const ts3Info = await getTS3Info();
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(ts3Info));
-
-    } catch (e) {
-
-      res.statusCode = 500;
-      res.setHeader("Content-Type", "text/plain");
-      res.end("Error connecting to TeamSpeak server / request limit reached.");
-
-    }
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(await requestServerInfo()));
   } else {
-
     res.statusCode = 404;
     res.setHeader("Content-Type", "text/plain");
     res.end("Not Found");
-
   }
 });
 
-const port = web_serverport;
-server.listen(port, () => {
-  console.log(`TS3-Query Webserver is running on port ${port}`);
-});
+server.listen(web_serverport, () => { console.log(`ServerQuery webserver is running on port ${web_serverport}.`) });
